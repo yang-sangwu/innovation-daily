2a84102b3c1e000d525d3f49bd69986f61955e01
yang-sangwu <2728771838@qq.com>
2023-06-08 10:27:15 +0800

    111

--- src/main/java/com/daily/config/MyCorsConfig.java
+++ src/main/java/com/daily/config/MyCorsConfig.java
@@ -5,22 +5,16 @@ package com.daily.config;
  * 全局跨域处理
  */
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration(proxyBeanMethods = false)
-public class MyCorsConfig {
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**");
-            }
-        };
+public class MyCorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
-
-
 }
